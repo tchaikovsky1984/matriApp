1caca5eb222b1682a9ae1b4746a92387aaadfd57
tchaikovsky1984 <anshulkumarg0re@gmail.com>
2024-05-19 15:40:54 +0530

    Update navbar
    added transition on scrolling

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,12 +1,25 @@
-import React from 'react';
+import React, { useState , useEffect } from 'react';
 import './Header.css';
 import LogoDiv from './LogoDiv.jsx';
 import SearchRow from './SearchRow.jsx';
 
 function Header() {
+  const [scroll, setScroll] = useState(false); 
+
+  const changeBackground = () => {
+    if(window.scrollY >= 314){
+      setScroll(true);
+    }
+    else{
+      setScroll(false);
+    }
+  }
+
+  window.addEventListener('scroll', changeBackground);
+
   return (
     <div className = 'headerContainer'>
-      <div className = "navBar">
+      <div className = {scroll ? 'navBar active' : 'navBar'}>
         <LogoDiv></LogoDiv>
         <a href = "localhost:5173">Sign Up</a>
         <a href = "localhost:5173">Login</a>
