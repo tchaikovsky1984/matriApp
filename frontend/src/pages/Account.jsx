a221f34bdbe7e517b7adc5882f5fb26708ec6398
tchaikovsky1984 <anshulkumarg0re@gmail.com>
2024-06-02 22:56:02 +0530

    Fixed page crash

--- frontend/src/pages/Account.jsx
+++ frontend/src/pages/Account.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import './Account.css';
-import '../../../backend/routes/userRoute.js';
-import { User } from '../../../backend/models/userModel.js';
 import { useState, useEffect} from 'react';
 
 function Account(props){ 
+
+  const subUser = () => {
+    console.log(`info`);
+  }
 
   return (
     <div className = 'appCont'>
@@ -12,18 +14,18 @@ function Account(props){
       <img src = {props.ax.image}/>
       <h2>{props.ax.displayName}</h2>
       <h3>{props.ax.email}</h3>
-    <form>
-      <label>Gender : </label>
-      <select>
+    <form className = 'f'>
+      <label >Gender : </label>
+      <select className = 's'>
         <option value = "true">Man</option>
         <option value = "false">Woman</option>
       </select>
       <label>Min Age : </label>
-      <input></input>
+      <input className = 'i'></input>
       <label>Max Age : </label>
-      <input></input>
+      <input className = 'i'></input>
       <label>Religion : </label>
-      <input></input>
+      <input className = 'i'></input>
     </form>
     <button onClick = {subUser}>Submit</button>
       </div>
